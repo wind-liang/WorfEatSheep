4c032aa7e2e89d3c8264c4d71e9fd38848cfd2b7
windliang <648917857@qq.com>
2017-08-22 09:25:03 +0800

    init

---
+++ board.js
@@ -0,0 +1,195 @@
+"use strict";
+var log = console.log.bind(console)
+var BOARD_WIDTH = 500;
+var BOARD_HEIGHT = 900;
+var SQUARE_SIZE = 100;
+var WORF_SIZE=75;
+var SHEEP_SIZE=50;
+var SQUARE_LEFT = (BOARD_WIDTH - SQUARE_SIZE * 4) /2;
+var SQUARE_TOP = (BOARD_HEIGHT - SQUARE_SIZE * 8) /2;
+
+var PIECE_NAME = [
+    "oo","worf","sheep"
+];
+var PIECE_NAME_CHN = [
+    "棋盘", "狼","羊"
+];
+
+// 棋子距离棋盘左边框的距离
+function SQ_X(sq,type) {
+    if(type==1){
+        return SQUARE_LEFT + FILE_X(sq)  * SQUARE_SIZE-WORF_SIZE/2;
+    }else {
+        return SQUARE_LEFT + FILE_X(sq)  * SQUARE_SIZE-SHEEP_SIZE/2;
+    }
+
+}
+
+// 棋子距离棋盘上边框的距离
+function SQ_Y(sq,type) {
+    if(type==1){
+        return SQUARE_TOP + RANK_Y(sq)  * SQUARE_SIZE-WORF_SIZE/2;
+    }else {
+        return SQUARE_TOP + RANK_Y(sq)  * SQUARE_SIZE-SHEEP_SIZE/2;
+    }
+}
+
+// Board对象的初始化代码，位于index.html中
+function Board(container, images) {
+    this.images = images;			// 图片路径
+    this.imgSquares = [];			// img数组，对应棋盘上的90个位置区域
+    this.pos = new Position();
+    this.pos.fromFen("9/9/2w/1sss/1s3s/1sss/2w/9/9 r - - 0 1");	// 根据FEN串初始化棋局
+
+    var style = container.style;
+    style.position = "relative";
+    style.width = BOARD_WIDTH + "px";
+    style.height = BOARD_HEIGHT + "px";
+    style.background = "url(" + images + "board2.jpg)";
+    var this_ = this;
+    for (var sq = 0; sq < 45; sq ++) {
+        // 遍历虚拟棋盘的45个点
+        // 1.判断该点是否位于真实棋盘
+        if (!IN_BOARD(sq)) {
+            this.imgSquares.push(null);
+            continue;
+        }
+
+        // 2.棋盘上的90个区域，每个区域都会定义一个对应的img标签
+        var img = document.createElement("img");
+        var style = img.style;
+        style.position = "absolute";
+
+        style.zIndex = 0;
+        // 3.每个棋盘区域都会绑定点击事件，参数sq_表示了具体点击的区域。（这里用到了“闭包”的知识吧）
+        img.onmousedown = function(sq_) {
+            return function() {
+                this_.clickSquare(sq_);
+            }
+        } (sq);
+
+        // 4.将定义好的img标签追加到html中
+        container.appendChild(img);
+
+        // 5.将img标签存储到imgSquares数组中，方便后续对该区域进行操作（比如，显示不同的棋子图片）
+        this_.imgSquares.push(img);
+    }
+
+    // 显示棋子图片
+    this.flushBoard();
+}
+
+// 点击棋盘的响应函数。点击棋盘（棋子或者空位置），就会调用该函数。sq_是点击的位置
+Board.prototype.clickSquare = function(sq_) {
+    var sq = sq_;						// 点击的位置
+    var pc = this.pos.squares[sq];	// 点击的棋子
+
+    if(pc==0&&this.sqSelected==0&&this.pos.sdPlayer==2){
+        this.addSheep(sq);
+        this.worfLive();
+        this.flushBoard();
+    }
+    if (pc && pc==this.pos.sdPlayer) {
+        // 点击了己方棋子，直接选中该子
+
+        // if (this.mvLast != 0) {
+        //     this.drawSquare(SRC(this.mvLast), false);
+        //     this.drawSquare(DST(this.mvLast), false);
+        // }
+        if(pc==2&&this.pos.sheeps>0){
+            return;
+        }
+        if (this.sqSelected) {
+            this.drawSquare(this.sqSelected, false);
+        }
+        this.drawSquare(sq, true);
+        this.sqSelected = sq;
+
+    } else if (pc==0&&this.sqSelected > 0) {
+        // 点击的不是己方棋子（对方棋子或者无子的位置），但有子选中了(一定是自己的子)，那么执行这个走法
+        this.addMove(MOVE(this.sqSelected, sq));
+        this.worfLive();
+        this.flushBoard();
+
+    }
+
+
+    log(this.pos.sheeps);
+    log(this.pos.liveSheeps);
+    log(this.pos.worfs);
+    if(this.pos.worfs==0){
+        alert("羊胜利！")
+    }
+}
+// 判断这步棋是否合法，如果合法，则执行这步棋
+Board.prototype.addMove = function(mv) {
+    // 判断这步棋是否合法
+    if (!this.pos.legalMove(mv)) {
+        return;
+    }
+    this.pos.movePiece(mv)
+    this.sqSelected = 0;
+    if(this.pos.sdPlayer==1){
+        this.pos.sdPlayer=2;
+    }else{
+        this.pos.sdPlayer=1;
+    }
+}
+Board.prototype.addSheep=function (sq) {
+    this.pos.addSheep(sq);
+    this.sqSelected = 0;
+    if(this.pos.sdPlayer==1){
+        this.pos.sdPlayer=2;
+    }else{
+        this.pos.sdPlayer=1;
+    }
+}
+Board.prototype.worfLive = function() {
+    this.pos.worfLive();
+}
+
+
+
+// 显示sq位置的棋子图片。如果该位置没棋子，则显示一张透明的图片。如果selected为true，则要显示棋子选中时的边框。
+Board.prototype.drawSquare = function(sq, selected) {
+    var img = this.imgSquares[sq];
+    var type=this.pos.squares[sq];
+    img.style.left = SQ_X(sq,type);
+    img.style.top = SQ_Y(sq,type);
+    img.style.width = SHEEP_SIZE;
+    img.style.height = SHEEP_SIZE
+    if(type==1){
+        img.style.width = WORF_SIZE;
+        img.style.height = WORF_SIZE;
+    }
+    img.src = this.images + PIECE_NAME[this.pos.squares[sq]] + ".gif";
+
+    img.width=img.style.width
+    img.height=img.style.height
+    if(selected){
+        if(1==this.pos.sdPlayer){
+            img.src = this.images + "bkm.gif";
+        }else{
+            img.src = this.images + "ra.gif";
+        }
+
+    }
+    if(sq==this.pos.diedWorf){
+        img.style.width = WORF_SIZE;
+        img.style.height = WORF_SIZE;
+        img.src = this.images + "bkm.gif";
+    }
+
+
+}
+
+// 重新显示棋盘上的棋子
+Board.prototype.flushBoard = function() {
+    for (var sq = 0; sq < 45; sq ++) {
+        if (IN_BOARD(sq)) {
+            this.drawSquare(sq);
+        }
+    }
+}
+
+
---
+++ position.js
@@ -0,0 +1,550 @@
+"use strict";
+
+// 棋子编号
+var PIECE_WORLF = 1;		// 狼
+var PIECE_SHEEP = 2;	// 羊
+
+
+// 棋盘范围
+var RANK_TOP = 0;
+var RANK_BOTTOM = 8;
+var FILE_LEFT = 0;
+var FILE_RIGHT = 4;
+var ADD_PIECE = false;	// 添加棋子
+var DEL_PIECE = true;	// 删除棋子
+// 辅助数组，用于判断棋子是否在棋盘上
+var IN_BOARD_ = [
+    0, 0, 1, 0, 0,
+    0, 1, 1, 1, 0,
+    1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1,
+    0, 1, 1, 1, 0,
+    0, 0, 1, 0, 0,
+];
+
+// 判断某位置是否在棋盘
+function IN_BOARD(sq) {
+    return IN_BOARD_[sq] != 0;
+}
+
+// 根据一维矩阵，获取二维矩阵行数
+function RANK_Y(sq) {
+    return Math.floor(sq / 5);
+}
+
+// 根据一维矩阵，获取二维矩阵列数
+function FILE_X(sq) {
+    return sq % 5;
+}
+
+// 将二维矩阵转换为一维矩阵
+function COORD_XY(x, y) {
+    return x + (y * 5);
+}
+
+function CHR(n) {
+    return String.fromCharCode(n);
+}
+
+function ASC(c) {
+    return c.charCodeAt(0);
+}
+// 获取走法的起点
+function SRC(mv) {
+    return mv & 63;
+}
+
+// 获取走法的终点
+function DST(mv) {
+    return mv >> 6;
+}
+
+// 将一个走法的起点和终点，转化为一个整型数字
+function MOVE(sqSrc, sqDst) {
+    return sqSrc + (sqDst << 6);
+}
+function CHAR_TO_PIECE(c) {
+    switch (c) {
+        case "w":
+            return PIECE_WORLF;
+        case "s":
+            return PIECE_SHEEP;
+        default:
+            return -1;
+    }
+}
+
+function Position() {
+
+}
+
+// 初始化棋局数组
+Position.prototype.clearBoard = function() {
+    this.sdPlayer = 1;	// 该谁走棋。1-狼；2-羊
+    this.squares = [];	// 这个就是一维棋局数组
+    this.sheeps=22;
+    this.liveSheeps=8;
+    this.worfs=2;
+    this.diedWorf=0;
+    this.killSheep=0;
+    for (var sq = 0; sq < 45; sq ++) {
+        this.squares.push(0);
+    }
+}
+
+// 将棋子pc添加进棋局中的sp位置
+// Position.prototype.addPiece = function(sq, pc) {
+//     this.squares[sq] = pc;
+// }
+
+// 通过FEN串初始化棋局
+
+Position.prototype.fromFen = function(fen) {
+    this.clearBoard();
+    var y = RANK_TOP;
+    var x = FILE_LEFT;
+    var index = 0;
+    if (index == fen.length) {
+        return;
+    }
+    var c = fen.charAt(index);
+    while (c != " ") {
+        if (c == "/") {
+            x = FILE_LEFT;
+            y ++;
+            if (y > RANK_BOTTOM) {
+                break;
+            }
+        } else if (c >= "1" && c <= "9") {
+            x = (ASC(c) - ASC("0"));
+        } else if (c=='s' || c=='w') {
+            if (x <= FILE_RIGHT) {
+                var pt = CHAR_TO_PIECE(c);
+                if (pt >= 0) {
+                    this.addPiece(COORD_XY(x, y), pt);
+                }
+                x++;
+            }
+        }
+        index ++;
+        if (index == fen.length) {
+            return;
+        }
+        c = fen.charAt(index);
+    }
+    index ++;
+    if (index == fen.length) {
+        return;
+    }
+
+}
+
+// 走一步棋
+Position.prototype.makeMove = function(mv) {
+    this.movePiece(mv);
+    return true;
+}
+
+// 根据走法移动棋子，删除终点位置的棋子，并将起点位置的棋子放置在终点的位置。
+Position.prototype.movePiece = function(mv) {
+    var sqSrc = SRC(mv);			// 起点位置
+    var sqDst = DST(mv);			// 终点位置
+    //var pc = this.squares[sqDst];	// 终点位置的棋子
+    // if (pc > 0) {
+    //     // 终点有棋子，要删除该棋子
+    //     this.addPiece(sqDst, pc, DEL_PIECE);
+    // }
+    var pc = this.squares[sqSrc];				// 起点位置的棋子
+    this.addPiece(sqSrc, pc, DEL_PIECE);	// 删除起点棋子
+    if(this.killSheep!=0){
+        this.addPiece(this.killSheep, 1, DEL_PIECE);	// 删除起点棋子
+        this.liveSheeps--;
+        this.killSheep=0
+    }
+    this.addPiece(sqDst, pc, ADD_PIECE);	// 将原来起点的棋子添加到终点
+}
+Position.prototype.addSheep = function(sq) {
+    if(this.sheeps>0){
+        this.addPiece(sq, 2, ADD_PIECE);	// 将原来起点的棋子添加到终点
+        this.sheeps--;
+        this.liveSheeps++;
+    }
+}
+// 如果bDel为false，则将棋子pc添加进棋局中的sp位置；如果bDel为true，则删除sp位置的棋子。
+Position.prototype.addPiece = function(sq, pc, bDel) {
+    // var pcAdjust;
+    this.squares[sq] = bDel ? 0 : pc;
+}
+
+Position.prototype.legalMoveWalk = function(src,dst) {
+    var sub=dst-src;
+    switch (src){
+        case 2:
+            return (sub==4 || sub==5 || sub ==6)
+        case 36:
+        case 6:
+            return (sub==-4 || sub==1 || sub==6)
+        case 37:
+        case 27:
+        case 23:
+        case 21:
+        case 17:
+        case 7:
+            return (sub==-5 || sub==-1 || sub==5 || sub==1)
+        case 38:
+        case 8:
+            return (sub==4 || sub==-1 || sub==-6)
+        case 10:
+            return (sub==5 || sub==1 ||sub==6)
+        case 13:
+        case 11:
+            return (sub==-1 || sub==1 || sub==5)
+        case 33:
+        case 31:
+            return (sub==-1 || sub==1 || sub==-5)
+        case 14:
+            return (sub==5|| sub==-1 ||sub==4)
+        case 15:
+        case 25:
+            return (sub==-5 || sub==5 ||sub==1)
+        case 29:
+        case 19:
+            return (sub==-5 || sub==5 ||sub==-1)
+        case 30:
+            return (sub==-5|| sub==1 || sub==-4)
+        case 34:
+            return (sub==-5|| sub==-1 ||sub==-6)
+        case 12:
+        case 16:
+        case 18:
+        case 22:
+        case 26:
+        case 28:
+        case 32:
+            return (sub==-5 || sub==-1 || sub==5 || sub==1 || sub==-6 || sub==-4 || sub==4 || sub==6)
+        case 20:
+            return (sub==-5 || sub==5 || sub==-4|| sub==6 || sub==1)
+        case 24:
+            return (sub==-5 || sub==5 || sub==4 || sub==-6 || sub==-1)
+        case 42:
+            return (sub==-4 || sub==-5 || sub ==-6)
+        default:
+            return false
+    }
+}
+Position.prototype.legalMoveJump = function(src,dst) {
+    var pcDst = this.squares[(src+dst)/2];
+    if(pcDst!=2){
+        return false;
+    }
+    this.killSheep=(src+dst)/2;
+    var sub=dst-src;
+    switch (src){
+        case 2:
+            return sub==10
+        case 36:
+            return (sub==2 || sub==-8)
+        case 6:
+            return (sub==12 || sub==2)
+        case 7:
+            return sub==10
+        case 17:
+        case 27:
+            return (sub==-10 || sub==-2 || sub==10 || sub==2)
+        case 37:
+            return -10
+        case 23:
+            return (sub==-10 || sub==-2 || sub==10 )
+        case 21:
+            return (sub==-10 || sub==2 || sub==10 )
+
+        case 8:
+            return (sub==-2 || sub==8)
+        case 38:
+            return (sub==-2 || sub==-12)
+        case 10:
+            return (sub==10 || sub==2 ||sub==12)
+
+        case 11:
+            return (sub==2 || sub==10)
+        case 13:
+            return (sub==-2 || sub==10)
+        case 33:
+            return (sub==-2 || sub==-10)
+        case 31:
+            return (sub==2 || sub==-10)
+        case 14:
+            return (sub==10|| sub==-2 ||sub==8)
+        case 15:
+            return (sub==10 ||sub==2)
+        case 25:
+            return (sub==-10 ||sub==2)
+        case 29:
+            return (sub==-10 ||sub==-2)
+        case 19:
+            return (sub==10 ||sub==-2)
+        case 30:
+            return (sub==-10|| sub==2 || sub==-8)
+        case 34:
+            return (sub==-10|| sub==-2 ||sub==-12)
+        case 12:
+            return (sub==-10 || sub==-2 || sub==10 || sub==2 || sub==8 || sub==12)
+        case 16:
+            return ( sub==10 || sub==2 || sub==-8 || sub==12)
+        case 18:
+            return (sub==-2 || sub==10  || sub==-12 || sub==8)
+        case 22:
+            return (sub==-10 || sub==-2 || sub==10 || sub==2 || sub==-12 || sub==-8 || sub==8 || sub==12)
+        case 26:
+            return (sub==-10  || sub==2 || sub==-8  || sub==12)
+        case 28:
+            return (sub==-10 || sub==-2 || sub==-12  || sub==8 )
+        case 32:
+            return (sub==-10 || sub==-2 || sub==10 || sub==2 || sub==-12 || sub==-8 )
+        case 20:
+            return (sub==-10 || sub==10 || sub==-8|| sub==12 || sub==2)
+        case 24:
+            return (sub==-10 || sub==10 || sub==8 || sub==-12 || sub==-2)
+        case 42:
+            return sub==-10
+        default:
+            return false
+    }
+}
+Position.prototype.worfLive = function() {
+    if(this.isWalk()){
+        this.worfs--;
+    }
+}
+Position.prototype.isWalkDetection = function() {
+    for (var i = 0; i < arguments.length; i++) {
+        if(!(this.squares[arguments[i]]==2)){
+            return false;
+        }
+    }
+    return true;
+}
+Position.prototype.isWalk = function() {
+    for (var sq = 0; sq < 45; sq ++) {
+        if(this.squares[sq]==1){
+            if(sq==this.diedWorf){
+                continue;
+            }
+            switch (sq){
+                case 2:
+                   if(this.isWalkDetection(6,7,8,12)){
+                       this.diedWorf=sq;
+                       return true;
+                   }
+                   break;
+                case 6:
+                    if(this.isWalkDetection(2,7,8,12,18)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 7:
+                    if(this.isWalkDetection(2,6,8,12,17)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 8:
+                    if(this.isWalkDetection(2,6,7,12,16)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 10:
+                    if(this.isWalkDetection(11,12,15,20,16,22)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 11:
+                    if(this.isWalkDetection(10,16,21,12,13)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 12:
+                    if(this.isWalkDetection(11,7,13,17,10,22,14,2,16,18,20,24)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 13:
+                    if(this.isWalkDetection(11,18,14,12,23)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 14:
+                    if(this.isWalkDetection(13,18,19,12,22,24)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                case 15:
+                    if(this.isWalkDetection(10,16,20,17)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 16:
+                    if(this.isWalkDetection(10,11,12,17,22,21,20,15,8,18,28,26)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 17:
+                    if(this.isWalkDetection(12,18,22,16,7,19,27,15)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 18:
+                    if(this.isWalkDetection(12,13,14,19,24,23,22,17,6,28,26,16)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 19:
+                    if(this.isWalkDetection(14,18,24,29,17)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                case 20:
+                    if(this.isWalkDetection(15,16,21,26,25,10,30,32,22,12)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 21:
+                    if(this.isWalkDetection(16,20,26,22,23,11,31)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 22:
+                    if(this.isWalkDetection(16,17,18,23,28,27,26,21,10,12,14,24,34,32,30,20)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 23:
+                    if(this.isWalkDetection(18,22,28,24,13,21,33)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 24:
+                    if(this.isWalkDetection(19,18,23,28,29,22,12,14,34,32)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 25:
+                    if(this.isWalkDetection(20,30,26,27)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 26:
+                    if(this.isWalkDetection(20,21,22,27,32,31,30,25,16,28,18,38)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                case 27:
+                    if(this.isWalkDetection(22,26,28,32,17,25,37,29)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 28:
+                    if(this.isWalkDetection(24,29,34,33,32,27,22,23,18,16,26,36)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 29:
+                    if(this.isWalkDetection(24,28,34,27,19)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 30:
+                    if(this.isWalkDetection(25,26,31,20,22,32)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 31:
+                    if(this.isWalkDetection(26,30,32,33,21)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 32:
+                    if(this.isWalkDetection(36,37,38,42,27,26,28,20,22,24,30,31,33,34)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 33:
+                    if(this.isWalkDetection(32,34,28,31,23)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 34:
+                    if(this.isWalkDetection(33,29,28,22,24,32)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 36:
+                    if(this.isWalkDetection(32,37,42,38,28)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 37:
+                    if(this.isWalkDetection(36,38,42,32,27)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 38:
+                    if(this.isWalkDetection(36,37,42,32,26)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                case 42:
+                    if(this.isWalkDetection(32,36,37,38)){
+                        this.diedWorf=sq;
+                        return true;
+                    }
+                    break;
+                default:
+                    return false
+            }
+        }
+    }
+
+}
+
+Position.prototype.legalMove = function(mv) {
+    var sqSrc = SRC(mv);						// 获取走法的起点位置
+    var pcSrc = this.squares[sqSrc];			// 获取起点位置的棋子
+    var sqDst = DST(mv);				// 获取走法的终点位置
+    if(pcSrc==1){
+        return this.legalMoveWalk(sqSrc,sqDst) || this.legalMoveJump(sqSrc,sqDst)
+    }
+    if(pcSrc==2){
+        return this.legalMoveWalk(sqSrc,sqDst)
+    }
+}
