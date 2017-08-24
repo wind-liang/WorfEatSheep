9e3ad8617b42924b7d7ab4490e5e3314733d1750
windliang <648917857@qq.com>
2017-08-24 22:45:20 +0800

    删掉之前多的

--- socket.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by liang on 2017/8/24.
- */
-var socket = io.connect('http://101.236.29.78:3000');
-socket.on('news', function (data) {
-    console.log(data);
-    socket.emit('my other event', { my: 'data' });
-});
